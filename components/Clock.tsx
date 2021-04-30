07cb143b856a082cd503fb942e3f34e72b0565ff
Well Oliveira <well.oliveira.snt@gmail.com>
2021-04-30 03:22:56 -0300

    refactor: Extract some files and hide env vars

---
+++ components/Clock.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable */
+
+import { Component } from 'react'
+
+export class Clock extends Component<{}, {time: string}> {
+  // @ts-ignore
+  constructor(props) {
+    // @ts-ignore
+    super(props)
+
+    //This declared the state of time at the very beginning
+    this.state = {
+      time: new Date().toLocaleTimeString(),
+    }
+  }
+
+  //This happens when the component mount and the setInterval function get called with a call back function updateClock()
+  componentDidMount() {
+    // @ts-ignore
+    this.intervalID = setInterval(() => this.updateClock(), 1000)
+  }
+
+  //This section clears setInterval by calling intervalID so as to optimise memory
+  componentWillUnmount() {
+    // @ts-ignore
+    clearInterval(this.intervalID)
+  }
+
+  //This function set the state of the time to a new time
+  updateClock() {
+    this.setState({
+      time: new Date().toLocaleTimeString(),
+    })
+  }
+
+  // @ts-ignore
+  render() {
+    return (
+      <div className="Time">
+        <p> {this.state.time}</p>
+      </div>
+    )
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,210 +1,91 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 import Head from 'next/head'
 import styled from 'styled-components'
+import { GlobalStyle } from '../components/GlobalStyle'
+import { Clock } from '../components/Clock'
+import { GoogleFonts } from 'next-google-fonts'
 
 const Container = styled.div`
   background-color: #2d2f33;
 `
+const Card = styled.div`
+  background-color: #2d2f38;
+  margin: 1rem;
+  flex-basis: 45%;
+  padding: 1.5rem;
+  text-align: left;
+  color: inherit;
+  text-decoration: none;
+  border: 1px solid #eaeaea;
+  border-radius: 10px;
+  transition: color 0.15s ease, border-color 0.15s ease;
+`
+
+const kelvinToCelsius = (temp: number): number => temp - 273.15
+const kelvinToCelsiusString = (temp: number): string => `${kelvinToCelsius(temp).toFixed(2)}˚ C`
 
 const Home: FC = () => {
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState< { name: string, main: {temp: number, temp_max: number, temp_min: number}} | undefined>(undefined)
+
+  useEffect(() => {
+    const fetchResult = async (): Promise<void> => {
+      const result = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?id=3449319&appid=${process.env.NEXT_PUBLIC_API_KEY}`
+      ).then((response) => response.json())
+
+      setData(result)
+    }
+
+    setLoading(true)
+    fetchResult().finally(() => setLoading(false))
+  }, [])
+
   return (
-    <Container className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className="card">
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-      {/* Remove this shit */}
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,
-            Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
+    <>
+      <GlobalStyle />
+      <Container className="container">
+        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" />
+        <Head>
+          <title>Next Home Viewer</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+          <Clock />
+          {
+            <div className="grid">
+              <Card className="card">
+                {loading && <>Loading</>}
+                {!loading && !!data && (
+                  <>
+                    <h3>{data?.name}</h3>
+                    <h4>{kelvinToCelsiusString(data?.main?.temp)}</h4>
+                    <h4>max {kelvinToCelsiusString(data?.main?.temp_max)}</h4>
+                    <h4>min {kelvinToCelsiusString(data?.main?.temp_min)}</h4>
+                    <p>
+                      Enjoy our day (or not){' '}
+                      <span role="img" aria-label="emoji-sun-glasses">
+                        😎
+                      </span>
+                    </p>
+                  </>
+                )}
+              </Card>
+            </div>
           }
-        }
-      `}</style>
-      <style jsx global>{`
-        html,
-        body {
-          color: lightgrey;
-          background-color: #2d2f33;
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
-            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-        }
+        </main>
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </Container>
+        <footer>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          </a>
+        </footer>
+      </Container>
+    </>
   )
 }
 
